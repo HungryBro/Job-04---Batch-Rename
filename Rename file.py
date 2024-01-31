3e59e0d3be39f2246fd55a2e3e3850685e11c944
Kritchai <loma.kcps@gmail.com>
2024-01-31 16:21:34 +0700

    update full

--- Rename file.py
+++ Rename file.py
@@ -1,2 +1,16 @@
 import os
 
+def rename_files (position, extension):
+
+    count = 1
+    for filename in os.listdir(position):
+        if filename.endswith(extension):
+            new_filename = str(count).zfill(3) + extension
+            old_path = os.path.join(position, filename)
+            new_path = os.path.join(position, new_filename)
+            os.rename(old_path, new_path)
+            count += 1
+
+position = "D:\Job-04---Batch-Rename"  # ตำแหน่งของ folder ที่เราต้องการจะเปลี่ยนชื่อ
+extension = ".jpg"  # ตั้ง file name extension ของ file ที่ต้องการจะเปลี่ยนชื่อ
+rename_files(position, extension)
